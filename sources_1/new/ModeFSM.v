12730f79e4ccd857fab3bedb824fb3a630e4e2c3
alexasdfasdfa <1723716131@qq.com>
2023-12-05 23:28:47 +0800

    1.Free mode 2.some basic display

--- sources_1/new/ModeFSM.v
+++ sources_1/new/ModeFSM.v
@@ -3,11 +3,13 @@
 module ModeFSM(
     input wire clk,         // Clock signal
     input wire rst,         // Reset signal
+    input wire[0:7] key_board_in,//Key board of playing signal
     input wire in,          // Mode signal
     output wire [1:0] mode, // State signal
     output wire signal      // Buzzer signal
 );
-
+    reg[2:0] play_mode[0:8];
+    
     // 4 States
     parameter MENU = 2'b00, FREE = 2'b01, 
                 AUTO = 2'b10, LERN = 2'b11;
@@ -31,7 +33,10 @@ module ModeFSM(
     // Return to MENU state when rst on POSITIVE edge
     always @(posedge clk or posedge rst) begin
         if (rst)
+        begin
             state <= MENU;
+            {play_mode[0],play_mode[1],play_mode[2],play_mode[3],play_mode[4],play_mode[5],play_mode[6],play_mode[7]}=24'b000_001_010_011_100_101_110_111;
+        end    
         else
             state <= next_state;
     end
@@ -40,12 +45,13 @@ module ModeFSM(
     // all to auto for now
     always @* begin
         case (state)
-            MENU: next_state = in ? AUTO : MENU;
-            FREE: next_state = in ? AUTO : MENU;
-            AUTO: next_state = in ? AUTO : MENU;
-            LERN: next_state = in ? AUTO : MENU;
+            MENU: next_state = in ?  FREE :  FREE;
+            FREE: next_state = in ?  FREE :  FREE;
+            AUTO: next_state = in ?  FREE :  FREE;
+            LERN: next_state = in ?  FREE :  FREE;
         endcase
     end
+    FREE_MODE FREE_MODE(clk, key_board_in,{play_mode[0],play_mode[1],play_mode[2],play_mode[3],play_mode[4],play_mode[5],play_mode[6],play_mode[7]},signal);
 
     // AUTO_Mode instantiation
     AUTO_Mode AUTO_Mode(
